f7fc3fc3a537c6bbc24ed478ab2f1e05c5d99dc8
Kris Zeynev <kriszeynev@yahoo.com>
2025-12-14 21:40:25 +0200

    07. Class counter

---
+++ 03. OOP in TypeScript/07. Class counter.ts
@@ -0,0 +1,19 @@
+class Counter {
+  private static count: number = 0;
+
+  static increment(): void {
+    this.count++;
+  }
+
+  static getCount(): number {
+    return this.count;
+  }
+}
+
+Counter.increment();
+
+Counter.increment();
+
+console.log(Counter.getCount());
+
+// Counter.count = 10; //Error: only accessible within Counte
